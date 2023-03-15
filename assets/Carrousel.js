c4ddc7486ee57d5c64b3f75240f7a61cf9ef6b92
mc1tana <michel-cardon@laposte.net>
2023-03-15 23:56:28 +0100

    feat : add logique of button bottom of carrousel

--- assets/Carrousel.js
+++ assets/Carrousel.js
@@ -5,22 +5,16 @@ const lis=document.querySelectorAll(".carousel-indicators button")
 let indexCarousel = 0;
 let timer=5000;
 let idInterval;
-
+function selectedPhoto(event){ 
+    indexCarousel=event.target.dataset.bsSlideTo;
+    changeActive()
+}
 function nextCarousel(){
     indexCarousel++;
     if(indexCarousel>imgs.length-1){
         indexCarousel=0;
     }  ;
-    lis.forEach((elt)=>{
-        elt.classList.remove("active");
-    });
-    imgs.forEach((elt)=>{
-            elt.classList.remove('active');
-
-    });
-    lis[indexCarousel].classList.add('active');
-    imgs[indexCarousel].classList.add("active");
- clearInterval(idInterval)
+   changeActive()
 }
 function prevCarousel(){
     console.log(lis)
@@ -29,6 +23,9 @@ function prevCarousel(){
     if(indexCarousel<0){
         indexCarousel=imgs.length-1;
     };
+    changeActive();
+}
+function changeActive(){
     lis.forEach((elt)=>{
         elt.classList.remove("active");
     });
@@ -36,9 +33,9 @@ function prevCarousel(){
             elt.classList.remove('active');
 
     });
-    console.log(indexCarousel)
+   
     lis[indexCarousel].classList.add('active');
     imgs[indexCarousel].classList.add("active");
-    clearInterval(idInterval);
+    clearInterval(idInterval)
 }
 idInterval= setInterval(nextCarousel, timer)
